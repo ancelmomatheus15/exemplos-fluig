cc13d0264b25da5e0b3f31b52fe7d45ae940f851
matheus.bonfim <matheus.bonfim@SPON010113245.sp01.local>
2017-11-14 16:25:48 -0200

    o

--- Exemplos/workflow/scripts/dg_desafio06Matheus.eventoAleatorio.js
+++ Exemplos/workflow/scripts/dg_desafio06Matheus.eventoAleatorio.js
@@ -1,3 +1,16 @@
-function eventoAleatorio(){
+//afterReleaseProcessVersion
+
+//Evento disparado após a criação ou modificação nas 
+//configurações de um determinado processo
+
+function eventoAleatorio(processXML){
 	
-}
+	    var pdv = getValue("ProcessDefinitionVersionDto");
+	    var processInfo = "\n### Nova versão de processo liberada: ###";
+	    processInfo += "\n User: " + getValue("WKUser");  
+	    processInfo += "\n processDescription: " + pdv.getProcessDescription();
+	    processInfo += "\n processId: " + pdv.getProcessId();
+	    processInfo += "\n version: " + pdv.getVersion();
+	    log.info(processInfo + "\n");
+	}
+	
